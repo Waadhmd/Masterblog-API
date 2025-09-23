94f15427b9b0eaff9ddfb09b57d62b993c92a22d
waadhmd <waad.hamoud94@gmail.com>
2025-09-23 21:54:58 +0200

    feat: add POST /api/posts and handle 400 errors for missing/invalid fields

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +10,37 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
-def get_posts():
+@app.route('/api/posts', methods=['GET','POST'])
+def handle_posts():
+    if request.method == 'POST':
+        data = request.get_json()
+        # Validate required fields
+        missing_fields = []
+        if not data or not data['title']:
+            missing_fields.append("title")
+        if not data or not data['content']:
+            missing_fields.append("content")
+
+        if missing_fields:
+            return jsonify({
+                "error": "Missing required fields",
+                "missing": missing_fields
+            }), 400
+
+        new_post = {
+            "id":max((post['id'] for post in POSTS),default=0)+1,
+            "title" : data.get('title'),
+            "content": data.get('content')
+        }
+        POSTS.append(new_post)
+        return jsonify(new_post),201
+
     return jsonify(POSTS)
+
+@app.errorhandler(400)
+def bad_request_error(error):
+    return jsonify({'error':'bad request , missing some data'}),400
+
 
 
 if __name__ == '__main__':
